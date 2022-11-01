4fae13f9c62925de9ce93b1e639637abdfc06de2
YOUR_NAME <YOUR_EMAIL>
2022-11-01 09:55:31 +0200

    project - my_test_site

--- my_test_site/urls.py
+++ my_test_site/urls.py
@@ -1,24 +1,13 @@
-"""my_test_site URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
-from crm.views import first_page, form_done
+from crm.views import first_page, thanks_page
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', first_page),
-    path('form_done/', form_done, name='form_done'),
-]
+                  path('admin/', admin.site.urls),
+                  path('', first_page),
+                  path('thanks/', thanks_page, name='thanks'),
+              ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
