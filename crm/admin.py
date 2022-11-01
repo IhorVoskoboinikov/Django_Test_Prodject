1b345b31ba8d9f6aa0e75849d1d1b1173883ba43
YOUR_NAME <YOUR_EMAIL>
2022-11-01 09:53:31 +0200

    crm app

--- crm/admin.py
+++ crm/admin.py
@@ -1,6 +1,28 @@
 from django.contrib import admin
-from crm.models import Order
+from crm.models import Order, StatusCrm, CommentCrm
 
-# Register your models here.
 
-admin.site.register(Order)
+class Comment(admin.StackedInline):
+    model = CommentCrm
+    fields = ('comment_dt', 'comment_text')
+    readonly_fields = ('comment_dt',)
+    extra = 0
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'order_status', 'order_name', 'order_phone', 'order_dt')
+    list_display_links = ('id', 'order_name')
+    search_fields = ('id', 'order_name', 'order_phone', 'order_dt')
+    list_filter = ('order_status',)
+    list_editable = ('order_status', 'order_phone',)
+    list_per_page = 5
+    list_max_show_all = 10
+    fields = ('id', 'order_status', 'order_dt', 'order_name', 'order_phone',)
+    readonly_fields = ('id', 'order_dt')
+    # fields class Comment
+    inlines = [Comment]
+
+
+admin.site.register(Order, OrderAdmin)
+admin.site.register(StatusCrm)
+admin.site.register(CommentCrm)
--- crm/views.py
+++ crm/views.py
@@ -1,26 +1,37 @@
 from django.shortcuts import render
 
-from crm.models import Order
+from cms.models import CmsSlider
 from crm.forms import OrderForm
+from crm.models import Order
+from price.models import PriceCard, PriceTable
+from telebot.send_message import send_telegram
 
-
-# Create your views here.
 
 def first_page(request):
-    object_list = Order.objects.all()
+    slider_list = CmsSlider.objects.all()
+    pc_1 = PriceCard.objects.get(pk=1)
+    pc_2 = PriceCard.objects.get(pk=2)
+    pc_3 = PriceCard.objects.get(pk=3)
+    price_table = PriceTable.objects.all()
     form = OrderForm()
-    return render(request, 'main.html', {
-        'object_list': object_list,
-        'form': form
-    })
+    dict_obj = {
+        'slider_list': slider_list,
+        'pc_1': pc_1,
+        'pc_2': pc_2,
+        'pc_3': pc_3,
+        'prise_table': price_table,
+        'form': form,
+    }
+    return render(request, 'index.html', dict_obj)
 
 
-def form_done(request):
-    name = request.POST['name']
-    phone = request.POST['phone']
-    order_to_save = Order(order_name=name, order_phone=phone)
-    order_to_save.save()
-    return render(request, 'form_done.html', {
-        'name': name,
-        'phone': phone
-    })
+def thanks_page(request):
+    if request.POST:
+        name = request.POST['name']
+        phone = request.POST['phone']
+        order_to_save = Order(order_name=name, order_phone=phone)
+        order_to_save.save()
+        send_telegram(tg_name=name, tg_phone=phone)
+        return render(request, 'thanks.html', {'name': name})
+    else:
+        return render(request, 'thanks.html')
