261df636287b374201a75b9292b67422d0782930
YOUR_NAME <YOUR_EMAIL>
2022-11-09 14:49:34 +0200

    start file to run install command

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@ pip install -r requirements.txt
 python manage.py makemigrations
 python manage.py migrate
 python data_to_db.py
-python manage.py createsuperuserenter
+python manage.py createsuperuser
 python manage.py runserver
